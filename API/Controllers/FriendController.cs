2964536b54a06447af14cd7e9d261e4ae9e4faba
Luisa <lulucabell@hotmail.com>
2019-11-24 16:28:41 -0300

    inputbinding created, apagamos meus metodos

---
+++ API/Controllers/FriendController.cs
@@ -0,0 +1,37 @@
+﻿using Core.Services;
+using System;
+using API.Services;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.ModelBinding;
+using System.Web.UI.WebControls;
+using Data.Services;
+using API.Models.InputFriend;
+
+namespace API.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/friend")]
+    public class FriendController : ApiController
+    {
+        private IFriend FriendService = ServiceLocator.GetInstanceOf<FriendRepository>();
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IHttpActionResult Create(InputFriendBindingModel input)
+        {
+            var friend = new InputFriendBindingModel().TransformInputIntoFriend(input);
+
+            var friendCreated = FriendService.Create(friend);
+            return Ok();
+        }
+       
+    }
+}
---
+++ API/Models/InputFriend/InputFriendBindingModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Core.Models;
+
+namespace API.Models.InputFriend
+{
+    public class InputFriendBindingModel
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public string Telefone { get; set; }
+        public string Email { get; set; }
+        public int[] Aniversario { get; set; }
+
+        public Friend TransformInputIntoFriend(InputFriendBindingModel input)
+        {
+            Friend mockFriend = new Friend() {
+                Aniversario = ConvertService(input.Aniversario),
+                Email = input.Email,
+                Nome = input.Nome,
+                Sobrenome = input.Sobrenome,
+                Telefone = input.Telefone
+                
+            };
+
+         
+            return mockFriend;
+        }
+
+        private DateTime ConvertService(int[] anoMesDia)
+        {
+            var ano = Convert.ToInt32(anoMesDia[0]);
+            var mes = Convert.ToInt32(anoMesDia[1]);
+            var dia = Convert.ToInt32(anoMesDia[2]);
+
+            return new DateTime(ano, mes, dia);
+        }
+    }
+}
