1b80eb90008c94bfc62a62462e41b8e8b2f6ea8e
Luisa <lulucabell@hotmail.com>
2019-11-24 15:55:23 -0300

    api services ifriend impl etc

---
+++ API/Services/ServiceLocator.cs
@@ -0,0 +1,22 @@
+﻿using Core.Services;
+using Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace API.Services
+{
+    public class ServiceLocator
+    {
+        private static Dictionary<Type, Type> Friend = new Dictionary<Type, Type>
+        {
+            [typeof(IFriend)] = typeof(FriendRepository)
+        };
+
+        internal static T GetInstanceOf<T>()
+        {
+            return Activator.CreateInstance<T>();
+        }
+    }
+}
---
+++ Data/Repository/FriendRepository.cs
@@ -0,0 +1,34 @@
+﻿using Core.Models;
+using Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Services
+{
+    public class FriendRepository : IFriend
+    {
+        public bool Create(Friend friend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Friend friend)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Friend> FindById(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Friend friend)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
