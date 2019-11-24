6bb176ec1df23aa23c374c4e849cbd391fb9ebd7
Carlos Henrique <carlos.hrq.rodrigues@gmail.com>
2019-11-24 18:34:37 -0300

    data

--- Data/Repository/FriendRepository.cs
+++ Data/Repository/FriendRepository.cs
@@ -8,45 +8,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Data.Services
-{
-    public class FriendRepository : IFriend
-    {
-        public bool Create(Friend friend)
-        {
-            try
-            {
+namespace Data.Services {
+    public class FriendRepository : IFriend {
+        public bool Create(Friend friend) {
+            try {
                 DatabaseContext.Create().Friends.Add(friend);
                 DatabaseContext.Create().SaveChanges();
                 return true;
-            }
-            catch (Exception e)
-            {
+            } catch(Exception e) {
                 Console.WriteLine(e.Message);
             }
             return false;
         }
 
-        public bool Delete(Friend friend)
-        {
-            throw new NotImplementedException();
+        public void Delete(int id) {
+            Friend f = DatabaseContext.Create().Friends.Find(id);
+            DatabaseContext.Create().Friends.Remove(f);
+            DatabaseContext.Create().SaveChanges();
         }
 
-        public Task<Friend> FindById(string id)
-        {
-            throw new NotImplementedException();
+        public Friend FindById(int id) {
+            var amigos = DatabaseContext.Create().Friends.ToList();
+
+            foreach(Friend friend in amigos) {
+                if(friend.Id == id) {
+                    return friend;
+                }
+            }
+            return null;
         }
 
-        public List<Friend> Friends()
-        {
+        public List<Friend> Friends() {
             var lista = new List<Friend>();
             lista = DatabaseContext.Create().Friends.ToList();
             return lista;
         }
 
-        public bool Update(Friend friend)
-        {
-            throw new NotImplementedException();
+        public bool Update(Friend friend) {
+            try {
+                DatabaseContext.Create().Entry<Friend>(friend).State = EntityState.Modified;
+                DatabaseContext.Create().SaveChanges();
+                return true;
+            } catch {
+                Console.WriteLine("error");
+            }
+            return false;
         }
     }
 }
