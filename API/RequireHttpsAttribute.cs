eb82d99463bfeab5085e185a5e5bdf660b37337f
Luisa <lulucabell@hotmail.com>
2019-11-25 20:21:13 -0300

    im tired, fixed SSL

--- API/RequireHttpsAttribute.cs
+++ API/RequireHttpsAttribute.cs
@@ -10,19 +10,19 @@ namespace API
 {
     public class RequireHttpsAttribute : AuthorizationFilterAttribute
     {
-    //    public override void OnAuthorization(HttpActionContext actionContext)
-    //    {
-    //        if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
-    //        {
-    //            actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
-    //            {
-    //                ReasonPhrase = "HTTPS Required for this call"
-    //            };
-    //        }
-    //        else
-    //        {
-    //            base.OnAuthorization(actionContext);
-    //        }
-    //    }
-}
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.RequestUri.Scheme != Uri.UriSchemeHttps)
+            {
+                actionContext.Response = new HttpResponseMessage(System.Net.HttpStatusCode.Forbidden)
+                {
+                    ReasonPhrase = "HTTPS Required for this call"
+                };
+            }
+            else
+            {
+                base.OnAuthorization(actionContext);
+            }
+        }
+    }
 }
